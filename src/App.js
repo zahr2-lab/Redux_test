64fddfe57b4c1512a0073909be4c53556a64075d
zahr2-lab <69382928+zahr2-lab@users.noreply.github.com>
2020-10-21 11:29:57 +0330

    Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Page from "./Components/Page/Page";
 import TopBar from "./Components/TopBar/TopBar";
 import "./styles.css";
